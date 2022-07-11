3dc6404a979e5c965c221f0d55c282229463fa39
yogs0ddhoth <blin7.webdev@gmail.com>
2022-07-11 17:44:05 -0400

    email/password validation, password encrypt added

--- app/models/User.py
+++ app/models/User.py
@@ -1,5 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
+
 from app.db import Base
-from sqlalchemy import Column, Integer, String, true
+
+import bcrypt
+salt = bcrypt.gensalt()
 
 class User(Base):
   __tablename__ = 'users'
@@ -7,3 +12,14 @@ class User(Base):
   username = Column(String(50), nullable = False)
   email = Column(String(50), nullable = False, unique = True)
   password = Column(String(100), nullable = False)
+
+  @validates('email')
+  def validate_email(self, key, email):
+    assert '@' in email
+    return email
+
+  @validates('password')
+  def validate_password(self, key, password):
+    assert len(password) > 4
+    # return encrypted password
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
