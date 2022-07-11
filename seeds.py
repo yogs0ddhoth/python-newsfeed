8494808e3f2142788f136ab4a9d65b8772ab29aa
yogs0ddhoth <blin7.webdev@gmail.com>
2022-07-11 18:24:03 -0400

    Comment seeds created

--- seeds.py
+++ seeds.py
@@ -1,4 +1,4 @@
-from app.models import User, Post
+from app.models import User, Post, Comment
 from app.db import Session, Base, engine
 
 # drop and rebuild tables
@@ -18,6 +18,7 @@ db.add_all(
   ]
 )
 db.commit()
+
 #  insert posts
 db.add_all(
   [
@@ -48,5 +49,35 @@ db.add_all(
     )
   ]
 )
+db.commit()
+
+# insert comments
+db.add_all([
+  Comment(
+    comment_text='Nunc rhoncus dui vel sem.', 
+    user_id=1, 
+    post_id=2
+  ),
+  Comment(
+    comment_text='Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', 
+    user_id=1, 
+    post_id=3
+  ),
+  Comment(
+    comment_text='Aliquam erat volutpat. In congue.', 
+    user_id=2, 
+    post_id=1
+  ),
+  Comment(
+    comment_text='Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', 
+    user_id=2, 
+    post_id=3
+  ),
+  Comment(
+    comment_text='In hac habitasse platea dictumst.', 
+    user_id=3, 
+    post_id=3
+  )
+])
 db.commit()
 db.close()
